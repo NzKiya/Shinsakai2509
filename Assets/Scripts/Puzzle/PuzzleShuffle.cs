cadd7065f38630d33ddd09de89167cdbe3333beb
nzkiya5492 <nzkiya5492@gmail.com>
2025-09-20 00:05:03 +0900

    コイン追加、シャッフルを別クラスに移動（未完成）

--- Assets/Scripts/Puzzle/PuzzleShuffle.cs
+++ Assets/Scripts/Puzzle/PuzzleShuffle.cs
@@ -1,18 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class PuzzleShuffle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    PuzzleManager _puzzleManager = default;
+    CoinManager _coinManager = default;
+    int _width = 10;
+    int _height = 8;
+    [SerializeField] int _shufflePrice = 10;
+
+    private void Start()
     {
-        
+        _puzzleManager = GetComponent<PuzzleManager>();
+        _coinManager = GetComponent<CoinManager>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ShufflePuzzle()
     {
-        
+        if (_puzzleManager.IsMoving || _coinManager.Coin < _shufflePrice) return;
+
+        _coinManager.Coin -= _shufflePrice;
+        Debug.Log(_coinManager.Coin + "coins left");
+        _puzzleManager.IsMoving = true;
+        List<GameObject> candidate = new List<GameObject>();
+
+        // フルーツの参照を削除
+        for (int x = 0; x < _width; x++)
+        {
+            for (int y = 0; y < _height; y++)
+            {
+                candidate.Add(_puzzleManager.PuzzleBoard[x, y]);
+
+                _puzzleManager.PuzzleBoard[x, y] = null;
+                //Debug.Log(x + ", " + y + " = " + _puzzleManager.PuzzleBoard[x, y]);
+            }
+        }
+
+        //フルーツの配置をランダムで決める
+        for (int x = 0; x < _width; x++)
+        {
+            for (int y = 0; y < _height; y++)
+            {
+                int r = Random.Range(0, candidate.Count);
+                //Debug.Log("random " + r);
+                var fruit = candidate[r];
+                //var fruit = _pool.GetFruit(r);
+                fruit.transform.localPosition = new Vector2(x, y);
+                _puzzleManager.PuzzleBoard[x, y] = fruit;
+                //fruit.GetComponent<FruitController>().PreviousPos = new Vector2(x, y);
+                //fruit.SetActive(false);
+                //fruit.SetActive(true);
+                candidate.RemoveAt(r);
+            }
+        }
+
+
+        _puzzleManager.CheckMatch();
     }
 }
